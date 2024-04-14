85b9c7ee0f30595905c5f1459f105b697918f21f
AdrianCDS <adriancodausi12@gmail.com>
2024-04-14 12:21:50 +0300

    added api key

--- src/main.jsx
+++ src/main.jsx
@@ -10,10 +10,14 @@ import {
 } from "@apollo/client";
 
 let apiUrl = import.meta.env.VITE_API_URL;
+let apiKey = import.meta.env.VITE_API_KEY;
 
 const client = new ApolloClient({
   uri: apiUrl,
   cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Basic ${apiKey}`,
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
