d3243718157b4ce83c44d8bb3864dca692afd5d5
mirunaCalugar <calugarmiruna6@gmail.com>
2024-05-14 19:24:06 +0300

    warning card

---
+++ src/pages/WarningCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import heartWarningImage from "../assets/heartwarning.png";
+
+//const pacient = pacientQueryResult.data?.getUserByPacientId;
+function WarningCard({ value, parameter, alertDate }) {
+  // Funcție pentru formatarea datei și orei
+  const formatDate = (date) => {
+    if (!date) return "Invalid Date";
+    const formattedDate = new Date(date);
+    return isNaN(formattedDate.getTime())
+      ? "Invalid Date"
+      : formattedDate.toLocaleString();
+  };
+
+  return (
+    <div className="bg-blue-200 p-4 rounded-md shadow-md flex items-center mb-4">
+      <img
+        src={heartWarningImage}
+        alt="Warning Icon"
+        className="w-20 h-20 mr-4"
+      />
+      <div className="flex flex-col">
+        <p className="text-red-800 font-semibold">
+          Warning: {parameter} exceeded the limit!
+        </p>
+        <p className="text-yellow-700">Current value: {value}</p>
+        <p className="text-yellow-700">
+          Suggestive message to avoid this situation...
+        </p>
+      </div>
+      <div className="ml-auto text-right">
+        <p className="text-gray-900">Alert Date: {formatDate(alertDate)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default WarningCard;
